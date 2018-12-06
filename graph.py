a1c91a494ba360b1b6338463a7bb5cbc9731f870
Pedro Henrique <pedrocarvalho@nca.ufma.br>
2018-12-06 19:57:40 -0300

    varias coisas

--- graph.py
+++ graph.py
@@ -1,17 +1,23 @@
+from collections import deque
+import numpy as np
+import copy
+
 class Vertice:
-	def __init__(self, id, cor = 'branco'):
+	def __init__(self, id, cor = 'b'):
 		self.id = id
 		self.cor = cor
 		self.adj = []
+		self.nivel = 0
+		self.pai = None
 	def __str__(self):
-		return self.id
+		return str(self.id)
 	def __repr__(self):
 		return str(self.id)
 
 	def setAdjacentes(self, adjacentes):
-		self.adj = adj.concat(adjacentes)
+		self.adj = self.adj.concat(adjacentes)
 	
-	def serAdjacente(self, adjacente):
+	def setAdjacente(self, adjacente):
 		self.adj.append(adjacente)
 
 
@@ -33,7 +39,6 @@ class Graph:
 
 	def getVertice(self,a):
 		for i in self.vertices:
-			print(i.id,a)
 			if i.id == a:
 				return i
 		return False
@@ -42,14 +47,191 @@ class Graph:
 		aux = True
 		origem = self.getVertice(a)
 		adj = self.getVertice(b)
-
+		
 		if (origem  == False):
 			origem = Vertice(a)
 			self.vertices.append(Vertice(a))
 		if(adj == False):
-			origem = Vertice(b)
+			adj = Vertice(b)
 			self.vertices.append(Vertice(b))
-		origem.serAdjacente(adj)
+		
+		origem = self.getVertice(a)
+		origem.setAdjacente(adj)
+	
+		aux = self.getVertice(a)
 
+		#print(origem.adj)
 		self.arestas.append(Aresta(origem,adj,peso))
 
+	def dfs(self,origem):
+		pilha = deque()
+		fila = []
+		cilco = ()
+
+		for i in range(len(self.vertices)):
+			self.vertices[i].cor = 'b'
+			self.vertices[i].nivel = 0
+
+		#print(origem.adj)	
+
+		fila.append(self.getVertice(origem))
+
+		for i in sorted(self.getVertices(), key=lambda adjacentes: int(adjacentes.id)):	
+			if(i not in fila):
+				fila.append(self.getVertice(i.id))
+
+		for i in fila:
+			origem = self.getVertice(i.id)
+			pilha.append(origem)
+
+
+
+			while(len(pilha)>0):
+				atual = pilha.pop()
+
+				origem = self.getVertice(atual.id)
+			
+
+				for i in sorted(self.getVertice(origem.id).adj, key=lambda adjacentes: int(adjacentes.id), reverse = True):
+
+					if self.getVertice(i.id).cor == "b":
+						pilha.append(i)
+						self.getVertice(i.id).pai = origem
+						self.getVertice(i.id).nivel = atual.nivel+1
+
+				origem.cor = 'c'
+				self.getVertice(origem.id).cor = 'c'
+
+				if(origem.pai != None):
+					origem.nivel = self.getVertice(origem.pai.id).nivel+1
+
+				
+
+		for i in self.vertices:
+			print("vertice :" +  str(i.id) +  " Pai : " +str( i.pai)+" Nivel :"+ str(i.nivel))
+
+
+
+	def bfs(self, origem):
+		fila = deque()
+		vertices = []
+		for i in range(len(self.vertices)):
+			self.vertices[i].cor = 'b'
+			self.vertices[i].nivel = 0
+			self.vertices[i].pai = None
+
+		aux = self.getVertice(origem)
+	
+		vertices.append(aux)
+		vertices = vertices + self.getVertices()
+		fila.append(aux)
+		#for k in vertices:
+			#fila.append(k)
+		print(origem)
+		while(len(fila)>0):
+			atual = fila.popleft()
+			self.getVertice(atual.id).cor = 'c'
+
+			adjacentes =  self.getVertice(atual.id).adj
+
+			for i in sorted(adjacentes ,key=lambda aux: int(aux.id), reverse = True):
+				aux = self.getVertice(i.id)
+				if(aux.cor !='c'):			
+					aux.cor = 'c'
+					aux.pai = atual
+					aux.nivel = atual.nivel+1
+					fila.appendleft(aux)
+			
+
+		for i in self.vertices:
+			print("vertice :" +  str(i.id) +  " Pai : " +str( i.pai)+" Nivel :"+ str(i.nivel))
+
+	def DistanceMatrix(self):
+		size = len(self.getVertices())
+		matrix = np.zeros((size,size),dtype=np.float64)
+		for i in self.getVertices():
+			self.bfs(i.id)
+
+			for k in self.getVertices():
+				matrix[i.id][k.id] = k.nivel
+			print(matrix)
+				
+		print(matrix)
+
+	def countCicles(self, target, node, cycles, pilha, visitado, punish):
+		if node == target:
+			if visitado[node] == 1:
+				#print(target)
+				for i in pilha:
+					if i.id == target:
+						cycles[i.id] += 1
+					else:
+						if punish[i.id] == 1:
+							cycles[i.id] -= 1
+				return True
+
+		pilha.append(self.getVertice(node))
+		#visitado[node] = 1
+
+		for i in sorted(self.getVertice(node).adj ,key=lambda aux: int(aux.id)):
+			if visitado[i.id] == 0:
+				visitado[i.id] = 1
+				result = self.countCicles(target, i.id, cycles, copy.copy(pilha), copy.copy(visitado), punish)
+
+
+		pilha.pop()
+		return False
+
+
+	def dfsCiclo(self,pilha,numciclo):
+		#print(numciclo)
+		print(pilha)
+		while (len(pilha) > 0 ):
+			aux = self.getVertice(pilha[len(pilha)-1].id)	
+			aux.cor = 'c'
+			print(pilha)
+			for k in aux.adj:
+				print(k.id)
+
+			pilha.pop()
+
+			for i in sorted(self.getVertice(aux.id).adj ,key=lambda aux: int(aux.id)):
+				#print(self.getVertice(i.id).cor,pilha)
+				#print(pilha,i)
+
+				if self.getVertice(i.id).cor == 'b':
+					self.getVertice(i.id).cor = 'c'
+					self.getVertice(i.id).nivel = aux.nivel+1
+					pilha.append(self.getVertice(i.id))
+					
+					self.dfsCiclo(copy.copy(pilha),numciclo)
+				elif self.getVertice(i.id).cor == 'c':
+					self.getVertice(i.id).cor = 'k'
+					for k in pilha:
+						if k.id == i.id:
+							print("ciclos")
+
+			#print(pilha)
+			#if(len(pilha)>0):
+			
+
+	def ciclos(self):
+		pilha = deque()
+		numciclo = 0
+		cycles = [0 for i in range(len(self.vertices))]
+		visited = [0 for i in range(len(self.vertices))]
+		punish = [1 for i in range(len(self.vertices))]
+		for i in range(len(self.vertices)):
+			self.countCicles(i, i, cycles, copy.copy(pilha), copy.copy(visited), punish)
+			punish[i] = 0
+			print(i, cycles)
+
+		c = 0
+	
+		for i in cycles:
+			if i > 0:
+				c += i
+
+		print(c)	
+		#print(self.dfsCiclo(pilha,numciclo))
+				
--- main.py
+++ main.py
@@ -2,9 +2,44 @@ import graph as g
 
 grafo = g.Graph()
 
+# grafo.insertAresta(2,0,10)
+# grafo.insertAresta(2,3,10)
+# grafo.insertAresta(2,4,10)
+# grafo.insertAresta(3,4,10)
+# grafo.insertAresta(0,4,10)
+# grafo.insertAresta(3,5,10)
+# grafo.insertAresta(4,2,10)
+# #grafo.insertAresta(5,3,10)
+# grafo.insertAresta(4,5,10)
+# grafo.insertAresta(0,1,10)
+# grafo.insertAresta(4,1,10)
+# grafo.insertAresta(5,1,10)
+
+
+grafo.insertAresta(0,1,10)
 grafo.insertAresta(1,2,10)
-
-grafo.insertAresta(1,3,10)
 grafo.insertAresta(2,3,10)
+grafo.insertAresta(3,4,10)
+grafo.insertAresta(4,2,10)
+grafo.insertAresta(4,5,10)
+grafo.insertAresta(5,6,10)
+grafo.insertAresta(6,7,10)
+grafo.insertAresta(7,5,10)
+#aux =  grafo.getVertice(2)
+#for i in aux.adj:
+#	i.cor = "joao"
+#	grafo.getVertice(i.id).cor = "asd"
+#
+#aux = grafo.getVertices()
+#for i in aux:
+#	print(i.cor)
 
-print(grafo.getVertices())
+#print(grafo.getVertices())
+
+#grafo.dfs(0)
+
+
+#print("\n\n bfs")
+#grafo.bfs(0)
+
+grafo.ciclos()
